8bd4157c49a68e40ff03556b93297fbdd3dae5a3
Jakub Nawłatyna <twblowek@gmail.com>
2022-06-07 19:04:21 +0200

    rysowanie

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,9 +32,10 @@ namespace ProjektOkienkowy
             this.Circle = new System.Windows.Forms.Button();
             this.Triangle = new System.Windows.Forms.Button();
             this.Parallelogram = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.whiteboard = new System.Windows.Forms.PictureBox();
             this.Complex = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.whiteboard)).BeginInit();
             this.SuspendLayout();
             // 
             // Circle
@@ -42,50 +43,62 @@ namespace ProjektOkienkowy
             this.Circle.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.Circle.Location = new System.Drawing.Point(814, 44);
             this.Circle.Name = "Circle";
-            this.Circle.Size = new System.Drawing.Size(119, 41);
+            this.Circle.Size = new System.Drawing.Size(127, 41);
             this.Circle.TabIndex = 0;
             this.Circle.Text = "Circle";
             this.Circle.UseVisualStyleBackColor = false;
-            this.Circle.Click += new System.EventHandler(this.button1_Click);
+            this.Circle.Click += new System.EventHandler(this.Circle_click);
             // 
             // Triangle
             // 
             this.Triangle.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.Triangle.Location = new System.Drawing.Point(814, 91);
             this.Triangle.Name = "Triangle";
-            this.Triangle.Size = new System.Drawing.Size(119, 38);
+            this.Triangle.Size = new System.Drawing.Size(127, 38);
             this.Triangle.TabIndex = 2;
             this.Triangle.Text = "Triangle";
             this.Triangle.UseVisualStyleBackColor = false;
+            this.Triangle.Click += new System.EventHandler(this.Triangle_Click);
             // 
             // Parallelogram
             // 
             this.Parallelogram.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.Parallelogram.Location = new System.Drawing.Point(814, 135);
             this.Parallelogram.Name = "Parallelogram";
-            this.Parallelogram.Size = new System.Drawing.Size(119, 49);
+            this.Parallelogram.Size = new System.Drawing.Size(127, 49);
             this.Parallelogram.TabIndex = 3;
             this.Parallelogram.Text = "Parallelogram";
             this.Parallelogram.UseVisualStyleBackColor = false;
+            this.Parallelogram.Click += new System.EventHandler(this.Parallelogram_Click);
             // 
-            // pictureBox1
+            // whiteboard
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(24, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(718, 558);
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
+            this.whiteboard.BackColor = System.Drawing.Color.White;
+            this.whiteboard.Location = new System.Drawing.Point(24, 12);
+            this.whiteboard.Name = "whiteboard";
+            this.whiteboard.Size = new System.Drawing.Size(718, 558);
+            this.whiteboard.TabIndex = 5;
+            this.whiteboard.TabStop = false;
             // 
             // Complex
             // 
             this.Complex.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.Complex.Location = new System.Drawing.Point(814, 190);
             this.Complex.Name = "Complex";
-            this.Complex.Size = new System.Drawing.Size(119, 48);
+            this.Complex.Size = new System.Drawing.Size(127, 48);
             this.Complex.TabIndex = 6;
             this.Complex.Text = "Complex_Shape";
             this.Complex.UseVisualStyleBackColor = false;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(814, 254);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(127, 34);
+            this.Clear.TabIndex = 7;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
             // 
             // Form1
             // 
@@ -93,14 +106,15 @@ namespace ProjektOkienkowy
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(1020, 582);
+            this.Controls.Add(this.Clear);
             this.Controls.Add(this.Complex);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.whiteboard);
             this.Controls.Add(this.Parallelogram);
             this.Controls.Add(this.Triangle);
             this.Controls.Add(this.Circle);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.whiteboard)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -110,8 +124,9 @@ namespace ProjektOkienkowy
         private System.Windows.Forms.Button Circle;
         private System.Windows.Forms.Button Triangle;
         private System.Windows.Forms.Button Parallelogram;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox whiteboard;
         private System.Windows.Forms.Button Complex;
+        private System.Windows.Forms.Button Clear;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,26 +2,54 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Drawing; // do rysowania 
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ProjektOkienkowy
 {
     public partial class Form1 : Form
     {
+        private System.Drawing.Graphics g; // tworzenie zmiennej Grapihics do rysowania 
+        private System.Drawing.Pen pen = new System.Drawing.Pen(Color.Aqua, 3); // tworzenie dlugopisa do rysowania 
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); // to sie robi samo - metoda wymagana do obsługi projektanta 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Circle_click(object sender, EventArgs e)
+        {
+            
+            g = whiteboard.CreateGraphics(); // tworzenie grafiki zmiennej na tablicy whiteboard
+            g.DrawEllipse(pen, 100, 100, 200, 200);
+
+        }
+
+        private void Triangle_Click(object sender, EventArgs e)
         {
 
+           
+            g = whiteboard.CreateGraphics(); // tworzenie grafiki zmiennej na tablicy whiteboard
+            Point[] points = new Point[] { new Point { X = 100, Y = 100 }, new Point { X = 40, Y = 60 }, new Point { X = 60, Y = 140 } };
+            g.DrawPolygon(pen, points);
+            
+
         }
 
-        
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            
+            whiteboard.Image = null;
+        }
+
+        private void Parallelogram_Click(object sender, EventArgs e)
+        {
+            g = whiteboard.CreateGraphics(); // tworzenie grafiki zmiennej na tablicy whiteboard
+            Point[] points = new Point[] { new Point { X = 100, Y = 100 }, new Point { X = 120, Y = 50 }, new Point { X = 190, Y = 50 }, new Point { X = 170, Y = 100 } };
+            g.DrawPolygon(pen, points);
+        }
     }
 }
