a01368982d080a94a8567d5d6a3c7f134a88751b
magda.lecka2002@gmail.com <magda.lecka2002@gmail.com>
2022-06-07 18:09:49 +0200

    hihi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,8 +53,9 @@ namespace ProjektOkienkowy
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Trójkąt";
+            this.button1.Text = "3some";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace ProjektOkienkowy
         {
 
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
