fa1ac4c0cb8c0bf02b7984def3b57e7820d590f8
magda.lecka2002@gmail.com <magda.lecka2002@gmail.com>
2022-06-07 19:40:49 +0200

    kolor pokna2

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -41,7 +41,7 @@ namespace ProjektOkienkowy
             // 
             // Clear
             // 
-            this.Clear.Location = new System.Drawing.Point(895, 232);
+            this.Clear.Location = new System.Drawing.Point(862, 232);
             this.Clear.Name = "Clear";
             this.Clear.Size = new System.Drawing.Size(127, 34);
             this.Clear.TabIndex = 13;
@@ -51,7 +51,7 @@ namespace ProjektOkienkowy
             // Complex
             // 
             this.Complex.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.Complex.Location = new System.Drawing.Point(895, 168);
+            this.Complex.Location = new System.Drawing.Point(862, 168);
             this.Complex.Name = "Complex";
             this.Complex.Size = new System.Drawing.Size(127, 48);
             this.Complex.TabIndex = 12;
@@ -61,7 +61,7 @@ namespace ProjektOkienkowy
             // whiteboard
             // 
             this.whiteboard.BackColor = System.Drawing.Color.White;
-            this.whiteboard.Location = new System.Drawing.Point(103, 22);
+            this.whiteboard.Location = new System.Drawing.Point(82, 22);
             this.whiteboard.Name = "whiteboard";
             this.whiteboard.Size = new System.Drawing.Size(718, 558);
             this.whiteboard.TabIndex = 11;
@@ -70,7 +70,7 @@ namespace ProjektOkienkowy
             // Parallelogram
             // 
             this.Parallelogram.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.Parallelogram.Location = new System.Drawing.Point(895, 113);
+            this.Parallelogram.Location = new System.Drawing.Point(862, 113);
             this.Parallelogram.Name = "Parallelogram";
             this.Parallelogram.Size = new System.Drawing.Size(127, 49);
             this.Parallelogram.TabIndex = 10;
@@ -80,7 +80,7 @@ namespace ProjektOkienkowy
             // Triangle
             // 
             this.Triangle.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.Triangle.Location = new System.Drawing.Point(895, 69);
+            this.Triangle.Location = new System.Drawing.Point(862, 69);
             this.Triangle.Name = "Triangle";
             this.Triangle.Size = new System.Drawing.Size(127, 38);
             this.Triangle.TabIndex = 9;
@@ -90,7 +90,7 @@ namespace ProjektOkienkowy
             // Circle
             // 
             this.Circle.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.Circle.Location = new System.Drawing.Point(895, 22);
+            this.Circle.Location = new System.Drawing.Point(862, 22);
             this.Circle.Name = "Circle";
             this.Circle.Size = new System.Drawing.Size(127, 41);
             this.Circle.TabIndex = 8;
@@ -101,7 +101,7 @@ namespace ProjektOkienkowy
             // 
             this.Back.Location = new System.Drawing.Point(12, 12);
             this.Back.Name = "Back";
-            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.Size = new System.Drawing.Size(54, 51);
             this.Back.TabIndex = 14;
             this.Back.Text = "button1";
             this.Back.UseVisualStyleBackColor = true;
@@ -111,6 +111,7 @@ namespace ProjektOkienkowy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(1020, 582);
             this.Controls.Add(this.Back);
             this.Controls.Add(this.Clear);
